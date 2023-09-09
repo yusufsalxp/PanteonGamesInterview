d15f231ad8819dd954323ab952790dbe7400be5f
Yusuf ŞAL <yusuf.sal.1996@gmail.com>
2023-09-09 04:27:44 +0300

    api project has been dockerized...

--- api/Program.cs
+++ api/Program.cs
@@ -101,12 +101,8 @@ internal class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
 
